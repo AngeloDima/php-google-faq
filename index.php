2b391a6b1f482ae6e7134566f9d99d8c8bf3b4da
Angelo <Angelodimauro91@gmail.com>
2022-10-17 22:57:30 +0200

    header

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<h1>ciao</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Google_form</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <h2>Google <h2>Privacy e termini</h2></h2>
+        </div>
+        <div class="info-menu">
+            <a href="">Introduzione</a>
+            <a href="">Norme sulla privacy</a>
+            <a href="">Termini di servizio</a>
+            <a href="">Tecnologie</a>
+            <a href="">Domande frequenti</a>
+        </div>
+        <hr>
+    </header>
+</body>
+</html>
